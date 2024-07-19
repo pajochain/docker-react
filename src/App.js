ddcd43b5726acb2ea98e3220491856bf34dc2924
Paulo Tanicala <tanicalapj@gmail.com>
2024-07-19 16:37:40 -0700

    modified text in App.js to match App.test.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Bitch Please!
+          learn react
         </a>
       </header>
     </div>
